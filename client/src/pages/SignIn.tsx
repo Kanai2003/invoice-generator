f18bba29271b3e74a5b2d7cd4151e685151826f1
Kanailal Manna <kanailalmanna2003@gmail.com>
2024-03-14 10:35:27 +0530

    Update page styles

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import LoginForm from '../components/LoginForm';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8  bg-slate-900 p-4 text-white">
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8  bg-slate-900 p-4 text-white">
       <div className="max-w-md w-full space-y-8 p-4">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold">Sign in to your account</h2>
--- server/src/app.js
+++ server/src/app.js
@@ -6,21 +6,12 @@ import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 
-// Configure CORS middleware
-const allowedOrigins = ['https://invoice-generator-black.vercel.app', 'http://localhost:3000', 'https://invoice-generator-kanailalmanna.vercel.app/', 'https://invoice-generator-git-main-kanailalmanna.vercel.app/', 'https://vercel.com/kanailalmanna/invoice-generator/1b2ABVf92jCCFZW7jKiD91uFtoPB']; // Add your Vercel app URL here
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // Allow credentials (cookies)
-};
 
-// Apply CORS middleware to all routes
-app.use(cors(corsOptions));
+
+app.use(cors({
+  origin: "https://invoice-generator-black.vercel.app",
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
